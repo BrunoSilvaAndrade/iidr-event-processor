436a7e3806de054870f846adc5ef57997686d6e4
BrunoSilvaAndrade <bruno.andrade@viavarejo.com.br>
2020-10-17 23:10:34 -0300

    Putting FunctionalInterface annotation on FieldParserAndSetter

--- src/main/java/br/com/viavarejo/iidr_event_processor/processor/FieldParserAndSetter.java
+++ src/main/java/br/com/viavarejo/iidr_event_processor/processor/FieldParserAndSetter.java
@@ -2,6 +2,7 @@ package br.com.viavarejo.iidr_event_processor.processor;
 
 import java.lang.reflect.Field;
 
+@FunctionalInterface
 public interface FieldParserAndSetter {
   void parseAndSet(Object object, Field field, String iidrValue) throws Exception;
 }
